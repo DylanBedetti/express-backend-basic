ffd38a21773028fa58af563709b2bf186c9b5567
Dylan Bedetti <dylanbedetti@gmail.com>
2021-01-06 13:14:24 +0000

    added postgres

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.get('/hi', (req, res) => {
+  res.send("helllllo");
 })
 
 app.listen(process.env.PORT, () => {
